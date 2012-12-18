29681648b900bf095e0ad6c9630c5578e71d042f
stoke <stoke95@gmail.com>
2012-12-18 20:54:56 +0100

    first

---
+++ test/test.js
@@ -0,0 +1,66 @@
+var expect = require('expect.js');
+
+var buffy = require('../'),
+    buffer = buffy();
+
+describe('#bind/flush', function() {
+  var test = false;
+
+  it('should create a function that buffer itself', function() {
+    (buffer.bind(function(cbl) {
+      test = true;
+      setTimeout(cbl, 500);
+    }))();
+  });
+
+  it('should be executed once flush is called', function(done) {
+    buffer.flush(function() {
+      expect(test).to.be.ok();
+      done();
+    });
+  });
+});
+
+describe('#buffer/flush', function() {
+  var test = false;
+
+  it('should bufferize a function', function() {
+    buffer.buffer(function(cbl) {
+      test = true;
+      setTimeout(cbl, 500);
+    });
+  });
+
+  it('should be executed once flush is called', function(done) {
+    buffer.flush(function() {
+      expect(test).to.be.ok();
+      done();
+    });
+  });
+});
+
+describe('#buffer&#bind/flush', function() {
+  var test1 = false,
+      test2 = false;
+
+  it('should buffer with #buffer and #bind', function() {
+    (buffer.bind(function(cbl) {
+      test1 = true;
+      setTimeout(cbl, 500);
+    }))();
+
+    buffer.buffer(function(cbl) {
+      test2 = true;
+      setTimeout(cbl, 500);
+    });
+  });
+
+  it('every buffered function should be executed once flush is called', function(done) {
+    buffer.flush(function(cbl) {
+      expect(test1).to.be.ok();
+      expect(test2).to.be.ok();
+      done();
+    });
+  });
+});
+
